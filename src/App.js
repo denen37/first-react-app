43aa841751cf3a6339fead36c9343eb48b700103
Denen <awardenen@gmail.com>
2024-06-24 11:17:33 +0100

    An app for scheduling tasks - first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import TodoItem from './components/TodoItem';
+import { useImmer } from "use-immer";
+
+let initialState = [
+  { id: 2, text: "Learn React", color: "black", isChecked: true },
+  { id: 3, text: "Learn Redux", color: "blue", isChecked: false },
+  { id: 1, text: "Build Something Cool", color: "red", isChecked: false }
+]
 
 function App() {
+  const [inputValue, setInputValue] = useState('');
+  const [todoList, updateTodoList] = useImmer(initialState);
+
+  const handleCheck = (id) => {
+
+    updateTodoList((list) => {
+      const todo = list.find(obj => obj.id === id)
+      todo.isChecked = !todo.isChecked
+    })
+  }
+
+  const handleCheckAll = (state) => {
+    updateTodoList((list) => {
+      list.forEach(item => item.isChecked = state)
+    })
+  }
+
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
+  }
+
+  const handleKeydown = (e) => {
+    const trimmedText = e.target.value.trim()
+
+    if (e.key === 'Enter' && trimmedText) {
+      let newId = Math.max(...todoList.map(item => item.id)) + 1
+
+      updateTodoList(list => {
+        list.push({ id: newId, text: trimmedText, color: 'black' })
+      })
+      setInputValue('');
+    }
+  }
+
+  const handleDelete = (id) => {
+    updateTodoList(() => todoList.filter(item => item.id !== id))
+  }
+
+  // useEffect(() => {
+
+  // }, [todoList])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-8/12 mx-auto py-4 bg-white min-h-screen flex flex-col">
+      <div className="bg-purple-600 py-3">
+        <h1 className="text-white text-4xl font-bold text-center">React Todo List</h1>
+      </div>
+      <div className="pt-4 pb-2 flex-1">
+        <h1 className="text-center text-[#b83f45] font-semibold text-2xl">TODOS</h1>
+        <div className="px-3">
+          <input placeholder="What needs to be done?" className="border border-gray-200 rounded py-2 px-3 text-lg text-gray-500"
+            onKeyDown={handleKeydown} value={inputValue} onChange={handleChange}
+          />
+        </div>
+        <div className='mt-2'>
+          {
+            todoList && todoList.map((item, index) => <div key={Math.random()}>
+              <TodoItem todo={item} onCheck={handleCheck} onDeleteClicked={handleDelete} />
+            </div>)
+          }
+        </div>
+      </div>
+      <div className='grid grid-cols-4 px-2 py-2 border-t-2 border-t-gray-200'>
+        <div className='flex flex-col items-center'>
+          <h2 className='font-bold text-base'>Action</h2>
+          <button className='text-white bg-blue-600 px-2 py-1  font-semibold rounded mb-2'
+            onClick={() => handleCheckAll(true)}
+          >Mark All Completed</button>
+          <button className='text-white bg-blue-600 px-2 py-1 font-semibold rounded'
+            onClick={() => handleCheckAll(false)}
+          >Clear completed</button>
+        </div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
     </div>
   );
 }
---
+++ src/components/TodoItem.js
@@ -0,0 +1,26 @@
+import check from '../images/check-mark-10120.png';
+import cancel from '../images/red-x-10340.png';
+
+const TodoItem = ({ todo, onCheck, onDeleteClicked }) => {
+
+    return (
+        <div className='flex items-center justify-between border-t-2 border-t-gray-200 px-2 py-2'>
+            <div className='flex items-center gap-2 '>
+                <div className='w-[30px] aspect-square rounded-full border border-gray-200 p-1 cursor-pointer'
+                    onClick={() => onCheck(todo.id)}>
+                    {
+                        todo.isChecked && <img src={check} className='w-full h-full' alt='' />
+                    }
+                </div>
+                <p style={{ color: todo.color }}>{todo.text}</p>
+            </div>
+            <div>
+                <div className='w-[30px] aspect-square p-1 cursor-pointer' onClick={() => onDeleteClicked(todo.id)}>
+                    <img src={cancel} className='w-full h-full' alt='' />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default TodoItem
